ba82854298f1038c62835d16cbce209cba3055fe
maywai <ricardoreparaqui@gmail.com>
2025-10-07 17:20:55 +0100

    white theme

--- config/nvim/lua/mizu/lazy/colors.lua
+++ config/nvim/lua/mizu/lazy/colors.lua
@@ -1,9 +1,9 @@
 local function ColorMyPencils(color)
-    color = color or "base16-black-metal"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none", fg = "none" })
 end
 
 return {
@@ -11,83 +11,14 @@ return {
     -- Other plugin configurations
 
     {
-        "metalelf0/base16-black-metal-scheme",
+        "kristijanhusak/vim-hybrid-material",
         config = function()
-            -- Moonfly does not require a setup function.
-            -- Set the colorscheme directly.
-            ColorMyPencils("base16-black-metal")
+            vim.g.transparent_background = 1
+            vim.o.background = "light"
+
+            ColorMyPencils("hybrid_reverse")
         end
     },
 
     -- Continue with other configurations
-
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            ColorMyPencils()
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true,
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true,
-                contrast = "",
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                style = "storm",
-                transparent = true,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    sidebars = "dark",
-                    floats = "dark",
-                },
-            })
-        end
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end
-    },
-
 }
-
